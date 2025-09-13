667a2009e01ac3f9201168f6ac833004dc380477
Shivansh Sahu <sahushivansh142@gmail.com>
2025-09-13 19:22:11 +0530

    ff

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,7 +29,7 @@ terraform {
   backend "s3" {
     bucket = "vprofileactions"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 
   required_version = "~> 1.6.3"
